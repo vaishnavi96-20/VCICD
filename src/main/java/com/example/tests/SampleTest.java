cfe2b57858426a4d29b698a802f01fbf5f68eb58
vaishnavi96-20 <71445130+vaishnavi96-20@users.noreply.github.com>
2025-02-24 20:29:52 +0800

    Update SampleTest.java

--- src/main/java/com/example/tests/SampleTest.java
+++ src/main/java/com/example/tests/SampleTest.java
@@ -1,1 +1,18 @@
 
+package com.example.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+    @Test
+    public void testGoogleSearch() {
+        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        System.out.println("Page title is: " + driver.getTitle());
+        driver.quit();
+    }
+}
